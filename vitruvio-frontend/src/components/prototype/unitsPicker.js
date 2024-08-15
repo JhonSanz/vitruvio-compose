83b7ef083cacb451d7889af66236a307b5bf6ad6
JhonSanz <ingjhonsanz@gmail.com>
2024-08-15 10:54:21 -0500

    Added scale component

---
+++ vitruvio-frontend/src/components/prototype/unitsPicker.js
@@ -0,0 +1,244 @@
+"use client";
+
+import { useState, useEffect } from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import fetchBackend from "@/utils/commonFetch";
+
+
+function UnitsSystem({
+  setSelectedUnit,
+  selectedUnit,
+  handleFormChange,
+  formValues
+}) {
+  const [unitTypes, setUnitTypes] = useState([]);
+  const [selectedType, setSelectedType] = useState(undefined);
+  const [availableUnits, setAvailableUnits] = useState([]);
+
+  async function getUnits() {
+    try {
+      const result = await fetchBackend(`/units/unit_by_type/${selectedType.id}`, "GET", {}, {}, "http://localhost:8001");
+      return result
+    } catch (error) {
+      console.error('Error fetching root nodes:', error);
+    }
+  }
+
+  async function getUnitTypes() {
+    try {
+      const result = await fetchBackend("/unit_type", "GET", {}, { filter_by: true }, "http://localhost:8001");
+      return result
+    } catch (error) {
+      console.error('Error fetching root nodes:', error);
+    }
+  }
+
+  useEffect(() => {
+    async function init() {
+      const unit_types = await getUnitTypes();
+      setUnitTypes(unit_types);
+    }
+    init();
+  }, []);
+
+  useEffect(() => {
+    async function init() {
+      if (selectedType) {
+        const units = await getUnits();
+        setAvailableUnits(units);
+      }
+    }
+    init();
+  }, [selectedType]);
+
+  return (
+    <Box>
+      <Box display="flex" marginBottom={5}>
+        <Box width="100%">
+          <h3>Tipos</h3>
+          <List style={{ border: "1px dotted gray", marginRight: "30px" }}>
+            {
+              unitTypes.map(item => (
+                <ListItem disablePadding>
+                  <ListItemButton selected={selectedType?.name === item.name} onClick={() => setSelectedType(item)}>
+                    <ListItemText primary={item.name} />
+                  </ListItemButton>
+                </ListItem>
+              ))
+            }
+          </List>
+        </Box>
+        <Box width="100%">
+          <h3>Unidades</h3>
+          <List style={{ border: "1px dotted gray", marginRight: "30px" }}>
+            {
+              availableUnits.map(item => (
+                <ListItem disablePadding>
+                  <ListItemButton selected={selectedUnit?.name === item.name} onClick={() => setSelectedUnit(item)}>
+                    <ListItemText primary={item.name} />
+                  </ListItemButton>
+                </ListItem>
+              ))
+            }
+          </List>
+        </Box>
+      </Box>
+      <Box display="flex" alignItems="center">
+        <Box mr={1}>
+          <TextField
+            size="small"
+            id="outlined-basic"
+            label="Value"
+            variant="outlined"
+            type='number'
+            name="value"
+            value={formValues["value"]}
+            onChange={(e) => handleFormChange(e)}
+          />
+        </Box>
+        <Box>{selectedUnit?.symbol}</Box>
+      </Box>
+    </Box>
+  )
+}
+
+function Scales({
+
+}) {
+  const [unitTypes, setUnitTypes] = useState([]);
+  const [selectedType, setSelectedType] = useState(undefined);
+
+  async function getUnitTypes() {
+    try {
+      const result = await fetchBackend("/unit_type", "GET", {}, { filter_by: false }, "http://localhost:8001");
+      return result
+    } catch (error) {
+      console.error('Error fetching root nodes:', error);
+    }
+  }
+
+  useEffect(() => {
+    async function init() {
+      const unit_types = await getUnitTypes();
+      setUnitTypes(unit_types);
+    }
+    init();
+  }, []);
+
+  return (
+    <Box>
+      <Box display="flex" marginBottom={5}>
+        <Box width="100%">
+          <h3>Tipos</h3>
+          <List style={{ border: "1px dotted gray", marginRight: "30px" }}>
+            {
+              unitTypes.map(item => (
+                <ListItem disablePadding>
+                  <ListItemButton selected={selectedType?.name === item.name} onClick={() => setSelectedType(item)}>
+                    <ListItemText primary={item.name} />
+                  </ListItemButton>
+                </ListItem>
+              ))
+            }
+          </List>
+        </Box>
+        {/* <Box width="100%">
+          <h3>Unidades</h3>
+          <List style={{ border: "1px dotted gray", marginRight: "30px" }}>
+            {
+              availableUnits.map(item => (
+                <ListItem disablePadding>
+                  <ListItemButton selected={selectedUnit?.name === item.name} onClick={() => setSelectedUnit(item)}>
+                    <ListItemText primary={item.name} />
+                  </ListItemButton>
+                </ListItem>
+              ))
+            }
+          </List>
+        </Box> */}
+      </Box>
+
+    </Box>
+  )
+}
+
+
+function UnitsPicker({ handleAddNewItem }) {
+  const [selectedUnit, setSelectedUnit] = useState(undefined);
+  const [formValues, setFormValues] = useState({});
+  const [itemType, setItemType] = useState("uis");
+
+  function handleFormChange(e) {
+    const copiedForm = { ...formValues };
+    copiedForm[e.target.name] = e.target.value;
+    setFormValues(copiedForm);
+  }
+
+  function handleSubmit() {
+    if (formValues.name && formValues.value) handleAddNewItem({
+      ...formValues, value: `${formValues.value}${selectedUnit.symbol}`
+    });
+  }
+
+  return (
+    <Box>
+      <Box display="flex" mr={1}>
+        <TextField
+          fullWidth
+          size="small"
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          name="name"
+          value={formValues["name"]}
+          onChange={(e) => handleFormChange(e)}
+        />
+        <FormControl size='small' fullWidth>
+          <InputLabel id="demo-simple-select-label">Mode</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={itemType}
+            label="itemType"
+            onChange={(e) => setItemType(e.target.value)}
+          >
+            <MenuItem value={"uis"}>Sistema de unidades</MenuItem>
+            <MenuItem value={"scales"}>Escalas</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <Box>
+        {
+          itemType === "uis" && <UnitsSystem
+            setSelectedUnit={setSelectedUnit}
+            selectedUnit={selectedUnit}
+            handleFormChange={handleFormChange}
+            formValues={formValues}
+          />
+        }
+        {
+          itemType === "scales" && <Scales
+
+          />
+        }
+      </Box>
+
+      <Box mt={2}>
+        <Button color="primary" size='small' variant="contained" onClick={() => handleSubmit()}>Ok</Button>
+      </Box>
+    </Box>
+  )
+}
+
+
+export default UnitsPicker;
